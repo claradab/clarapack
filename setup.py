e605e340bf63e9d0c51adfd7ec32dd30af4c1a59
claradab <56487482+claradab@users.noreply.github.com>
2019-11-19 19:23:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-clarapath", 
+    name="example-pkg-claradab", 
     version="0.0.1",
     author="Clara",
     author_email="clara@example.com",
